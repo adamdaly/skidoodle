d2e25e370fc9c77f504000d873e02c7e1be88e0e
Adam Daly <adam.daly@BEL-MC-240719A.local>
2025-04-29 14:22:20 +0100

    Added E2E tests using playwright

--- testing/e2e/src/fixtures/create-animation.fixture.ts
+++ testing/e2e/src/fixtures/create-animation.fixture.ts
@@ -4,13 +4,19 @@ import { CustomFixtures } from "../types";
 import { test as base } from "./sign-in.fixture";
 
 export const test = base.extend<CustomFixtures>({
-  animation: async ({ page }, use) => {
+  animation: async ({ page }, use, testInfo) => {
     const dashboard = new Dashboard(page);
     await dashboard.goto({ waitUntil: "load" });
 
     const responsePromise = dashboard.page.waitForResponse(
       (res) => res.url().includes("/animations") && res.status() === 201
     );
+
+    const screenshot = await page.screenshot();
+    await testInfo.attach("screenshot", {
+      body: screenshot,
+      contentType: "image/png",
+    });
 
     await dashboard.completeAnimationCreateForm();
 
